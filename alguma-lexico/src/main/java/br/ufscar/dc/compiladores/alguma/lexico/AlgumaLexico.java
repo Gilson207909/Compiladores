247fbd87730618051e1b8df344fb7a1ae5aeb90d
Gilson207909 <115376819+Gilson207909@users.noreply.github.com>
2023-06-11 13:33:26 -0300

    Add files via upload

---
+++ alguma-lexico/src/main/java/br/ufscar/dc/compiladores/alguma/lexico/AlgumaLexico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ufscar.dc.compiladores.alguma.lexico;
+
+/**
+ *
+ * @author Gilson Prado Jr
+ */
+public class AlgumaLexico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
